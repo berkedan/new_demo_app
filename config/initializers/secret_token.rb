2094c1f9b366dd895ecd1953ee114253cab7239c
Dan Berke <danb@mobilibuy.com>
2014-04-29 18:46:02 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewDemoApp::Application.config.secret_token = '179aec09fa5b04575fbf3d2a6a905035b2e085abfdee2372081ae39a3daed88de84a82ac99a36d9a4e4d2913a75fc04e7a6237f80d8b0f2b81d31a2b3c734720'
